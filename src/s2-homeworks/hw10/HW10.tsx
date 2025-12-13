5e19cb9be700d575052dc586d5bb27a0b1839527
valeryia <lera020302@gmail.com>
2025-12-13 20:23:40 +0300

    10hw

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -10,17 +10,20 @@ import {Loader} from './Loader'
 * 1 - в файле loadingReducer.ts дописать типы и логику
 * 2 - получить isLoading из редакса
 * 3 - дописать функцию setLoading
-* 4 - сделать стили в соответствии с дизайном
 * */
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    // const isLoading = false
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+    const dispatch = useDispatch()
+
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
-        // setTimeout
+            dispatch(loadingAC(true))
+            setTimeout(() => {
+                dispatch(loadingAC(false))
+            }, 1500)
     }
 
     return (
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,16 @@
-const initState = {
+type InitStateType = {
+    isLoading: boolean
+}
+
+const initState: InitStateType = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: InitStateType = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return { ...state, isLoading: action.isLoading }
         default:
             return state
     }
